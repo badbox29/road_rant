d6332b57f67da920730a27a66e4d3f7d280d0d2e
badbox29 <96398618+badbox29@users.noreply.github.com>
2026-06-13 00:25:22 -0400

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -135,7 +135,7 @@ const Auth = (() => {
     );
     return crypto.subtle.deriveKey(
       { name: 'HKDF', hash: 'SHA-256',
-        salt: enc.encode('reverence-hmac-v1'),
+        salt: enc.encode('road-rant-hmac-v1'),
         info: enc.encode('request-signing') },
       keyMat,
       { name: 'HMAC', hash: 'SHA-256' },
